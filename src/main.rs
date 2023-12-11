16cf984ea48400b63d94bf55710995165f97c2f9
Cormanz <mrccay12@gmail.com>
2023-12-11 09:47:43 -0500

    Update to monster-chess v0.0.20

--- src/main.rs
+++
@@ -1,9 +0,0 @@
-use monster_chess::games::chess::Chess;
-use monster_ugi::{ugi::run_ugi, random::RandomEngine, engine::Engine};
-
-pub fn main() {
-    run_ugi(Engine {
-        game: Chess::create(),
-        behavior: Box::new(RandomEngine(rand::thread_rng()))
-    });
-}
